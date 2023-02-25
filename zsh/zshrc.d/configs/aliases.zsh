b3891c66511ebf6be3f72bf99462c06a748edd0a
denperr <dperrone@redhat.com>
2023-02-25 06:01:24 -0500

    Add MacOS zsh configs

--- zsh/zshrc.d/configs/aliases.zsh
+++ zsh/zshrc.d/configs/aliases.zsh
@@ -1,5 +1,32 @@
-alias hey="echo hey there"
+
 alias vim='nvim'
+
+# User specific aliases and functions
+
+# Editor aliases are set in env_vars.sh file.
+
+alias ls='ls --color=auto'
+alias ll='ls -alh --color=auto'
+alias lh='ls -lh --color=auto'
+alias grep='grep --color=auto'
+alias h='history | grep'
+alias syshosts='cat /etc/hosts'
+alias cal='cal -m'
+alias pip-update='pip3 list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1 | xargs -n1 pip3 install -U'
 alias reload='source ~/.zshrc'
 alias config='nvim ~/.zshrc'
-alias ll='ls -alh --color'
+
+# vimwiki and notes
+alias wiki='nvim $HOME/wiki/index.md'
+alias nn='~/bin/new_note.sh'
+
+# git aliases
+alias ga='git add .'
+alias gc='git commit -a -m'
+alias gp='git push'
+alias gs='git status'
+alias gcb='git checkout -b'
+
+# zathura aliases
+alias zathura-bmarks-edit='vim ~/.local/share/zathura/bookmarks'
+alias zathura-bmarks-view='more ~/.local/share/zathura/bookmarks'
--- zsh/zshrc.d/configs/prompt.zsh
+++ zsh/zshrc.d/configs/prompt.zsh
@@ -5,15 +5,25 @@ autoload -Uz compinit vcs_info
 precmd() {
     vcs_info
     setopt prompt_subst
-    if [[ -z ${vcs_info_msg_0_} ]]; then
-        # Short prompt outside of a git repository.
-	    #PROMPT="%(?.%B%F{green} ❱.%B%F{red} ❱)%f%b "
-	    PROMPT="[%n][%m][%1~]%(?.%B%F{green} ❱.%B%F{red} ❱)%f%b "
-	    RPROMPT='%B%F{magenta}%2~%f%b'
+    if [[ "$EUID" -ne 0 ]]; then
+        if [[ -z ${vcs_info_msg_0_} ]]; then
+            # Short prompt outside of a git repository.
+            # PROMPT="[%n][%1~]%(?.%B%F{green} ❱.%B%F{red} ❱)%f%b "
+            # RPROMPT='%B%F{magenta}%2~%f%b'
+            PROMPT="[%B%F{green}%n%f%b][%B%F{magenta}%1~%f%b]%(?.%B%F{green} ❱.%B%F{red} ❱)%f%b "
+        else
+            # Longer prompt inside of a git repository.
+            PROMPT="[%B%F{magenta}%1~%f%b]%B%F${vcs_info_msg_0_}%b%f%(?.%B%F{green}❱.%B%F{red}❱)%f%b "
+            # RPROMPT='%B%F{magenta}%2~%f%b'
+        fi
     else
-        # Longer prompt inside of a git repository.
-	    PROMPT="${vcs_info_msg_0_}%(?.%B%F{green}❱.%B%F{red}❱)%f%b "
-	    RPROMPT='%B%F{magenta}%2~%f%b'
+        if [[ -z ${vcs_info_msg_0_} ]]; then
+            # Short prompt outside of a git repository.
+            PROMPT="[%B%F{red}%n%f%b][%B%F{magenta}%1~%f%b]%(?.%B%F{green} ❱.%B%F{red} ❱)%f%b "
+        else
+            # Longer prompt inside of a git repository.
+            PROMPT="[%B%F{red}%1~%f%b]%B%F${vcs_info_msg_0_}%b%f%(?.%B%F{green}❱.%B%F{red}❱)%f%b "
+        fi
     fi
 }
 zstyle ':vcs_info:git:*' formats '%F{magenta}(%b)%f '
