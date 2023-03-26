c2f04d29c2ffdf1d0c9dd6ad30c7bb67773ddf55
dennis-perrone <dennis.perrone@gmail.com>
2023-03-26 07:25:53 -0400

    Update config file to work with Lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,12 +7,4 @@ require('user.keymaps')                                   -- Define keymaps
 require('user.lualine')                                   -- Lualine configuration
 require('user.nvimtree')                                  -- Nvim Tree configuration
 require('user.vimwiki')                                   -- Vimwiki configuration
-
-
---require('user.bootstrap')                               -- Load bootstrap file for Packer
---require('user.plugins')                                 -- Define and install plugins
---require('user.maps')                                    -- Define key maps
---require('user.settings')                                -- General settings
---require('user.lualine')                                 -- Lualine configuration
---require('user.vimwiki')                                 -- Vimwiki configuration
---require('user.nvim-tree')
+require('user.markdown-preview')                          -- Markdown Preview configuration
