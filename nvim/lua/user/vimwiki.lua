e3d20dba62a99a1497d2e3c3da2ebf02d02017b7
denperr <dperrone@redhat.com>
2023-05-20 10:32:11 -0400

    Replace nvim configs

--- nvim/lua/user/vimwiki.lua
+++
@@ -1,9 +0,0 @@
--- VimWiki Configuration
-
-vim.g.vimwiki_list = {{path = '~/wiki/', syntax = 'markdown', ext = '.md'}}    -- Define vimwiki list
-vim.g.vimwiki_markdown_link_ext = 1                                            -- Ensure all new links have .md extension
-
-vim.wiki_1 = {}
-vim.wiki_1.path = '~/wiki/'
-vim.wiki_1.syntax = 'markdown'
-vim.wiki_1.ext = '.md'
--- nvim/lua/user/wiki.lua
+++
@@ -1,6 +0,0 @@
--- Wiki.vim Configuration
-
-vim.g.wiki_root = '~/wiki'
-vim.g.wiki_filetypes = {'md'}
-vim.g.wiki_link_extension = '.md'
-vim.g.wiki_link_target_type = 'md'
